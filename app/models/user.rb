f7bef3ab20501e166950e734462efd570d0cd0c8
mahesh Paruchuri <saisivap289@gmail.com>
2019-10-30 00:13:06 +0700

    Added validation to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  # validates_presence_of :name,length: {minimum: 5, maximum: 15}
+  #  validates_length_of :name, minimum: 5, maximum: 15, allow_blank: true
+  validates :name,
+            presence: true,
+            length: {minimum:5,maximum: 25},
+            on: :create,
+            allow_nil: false
   has_many :message1
 end
